d96c293e0e731c0fe67c1b73b8aa27811f40042b
FritzEreneta <fereneta885@gmail.com>
2019-09-08 12:06:11 +0800

    Add getInput function

---
+++ main.js
@@ -0,0 +1,9 @@
+let firstName = document.getElementById('firstName');
+let secondName = document.getElementById('#firstName');
+
+function getInput(){
+
+    let input = document.querySelector('#firstName').Value;
+    let outPut = document.querySelector('#outPut');
+    outPut.setAttribute('value', input);
+}
